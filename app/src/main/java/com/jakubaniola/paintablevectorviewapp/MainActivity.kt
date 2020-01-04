f9ce8e9b9848b20c3d1b840cc61ce7276fe42360
Jakub Anioła <jakub.aniola@rsqtechnologies.com>
2020-01-04 15:12:53 +0100

    update example app with all functionalities

--- app/src/main/java/com/jakubaniola/paintablevectorviewapp/MainActivity.kt
+++ app/src/main/java/com/jakubaniola/paintablevectorviewapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.jakubaniola.paintablevectorviewapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.jakubaniola.paintablevectorview.PaintType
 import com.jakubaniola.paintablevectorview.PaintableVectorView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var paintableView: PaintableVectorView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +17,49 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        val view = PaintableVectorView(this, R.drawable.ic_car, PaintType.PAINT_GROUP)
-        view.paintColor = resources.getColor(R.color.colorPrimaryDark)
-        drawableBoxLayout.addView(view)
+        paintableView = PaintableVectorView(
+            this,
+            R.drawable.ic_car,
+            PaintType.PAINT_GROUP,
+            resources.getColor(R.color.blue)
+        )
+        setupPaintableView()
+        setupResetButton()
+        setupPaintTypeButtons()
+        setupPaintColorButtons()
+    }
+
+    private fun setupResetButton() {
+        resetColorsButton.setOnClickListener {
+            paintableView.resetColors()
+        }
+    }
+
+    private fun setupPaintTypeButtons() {
+        drawPathButton.setOnClickListener {
+            paintableView.paintType = PaintType.PAINT_PATH
+        }
+        drawGroupButton.setOnClickListener {
+            paintableView.paintType = PaintType.PAINT_GROUP
+        }
+    }
+
+    private fun setupPaintColorButtons() {
+        paintBlueButton.setOnClickListener {
+            paintableView.paintColor = resources.getColor(R.color.blue)
+        }
+        paintRedButton.setOnClickListener {
+            paintableView.paintColor = resources.getColor(R.color.red)
+        }
+        paintGreenButton.setOnClickListener {
+            paintableView.paintColor = resources.getColor(R.color.green)
+        }
+        paintYellowButton.setOnClickListener {
+            paintableView.paintColor = resources.getColor(R.color.yellow)
+        }
+    }
+
+    private fun setupPaintableView() {
+        paintableBoxLayout.addView(paintableView)
     }
 }
